a5883271facbc7ab0294e7712539d6f3baa2d103
Dheeraj kumar <dheerajkumarr005@gmail.com>
2025-12-31 22:08:49 +0530

    Upgrade to puppeteer-extra with Stealth Plugin to resolve blocking issues

--- backend/scraper.js
+++ backend/scraper.js
@@ -1,4 +1,6 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const fs = require('fs');
 const path = require('path');
 const { URL } = require('url');
